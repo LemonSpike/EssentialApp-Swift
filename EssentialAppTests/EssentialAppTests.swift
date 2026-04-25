1bd4da5a3886fdaa76b004c1b86fb2bd2e740c0d
Kasetti, Pranav (Developer - Sky) <165320713+Pranav-Kasetti-ts7_nbcuni@users.noreply.github.com>
2026-04-25 18:15:40 +0100

    Add empty EssentialApp project.

---
+++ EssentialAppTests/EssentialAppTests.swift
@@ -0,0 +1,5 @@
+import XCTest
+@testable import EssentialApp
+
+final class EssentialAppTests: XCTestCase {
+}
---
+++ EssentialAppUITests/EssentialAppUITests.swift
@@ -0,0 +1,8 @@
+import XCTest
+
+final class EssentialAppUITests: XCTestCase {
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+}
