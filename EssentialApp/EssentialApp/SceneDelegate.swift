ae8fd14cc842af6799de19f8d8c9714c053b7ade
Pranav Kasetti <pranavkasetti@gmail.com>
2026-06-28 16:57:05 +0100

    Displays cached feed on launch when customer has no connectivity.

--- EssentialApp/EssentialApp/SceneDelegate.swift
+++ EssentialApp/EssentialApp/SceneDelegate.swift
@@ -10,19 +10,62 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
   func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
     guard let _ = (scene as? UIWindowScene) else { return }
     
-    let urlSession = URLSession(configuration: .ephemeral)
     let remoteURL = URL(string: "https://ile-api.essentialdeveloper.com/essential-feed/v1/feed")!
-    let remoteClient = URLSessionHTTPClient(session: urlSession)
+    let remoteClient = makeRemoteClient()
     let remoteFeedLoader = RemoteFeedLoader(
       url: remoteURL,
       client: remoteClient
     )
     let remoteImageLoader = RemoteFeedImageDataLoader(client: remoteClient)
     
-    window?.rootViewController = FeedUIComposer.feedComposedWith(
-      feedLoader: remoteFeedLoader,
-      imageLoader: remoteImageLoader
+    let localStoreURL = NSPersistentContainer
+      .defaultDirectoryURL()
+      .appendingPathComponent("feed-store.sqlite")
+    let localStore = try! CoreDataFeedStore(storeURL: localStoreURL)
+    let localFeedLoader = LocalFeedLoader(
+      store: localStore,
+      currentDate: Date.init
     )
+    let localImageLoader = LocalFeedImageDataLoader(store: localStore)
+    
+    let compositeFeedLoader = FeedLoaderWithFallbackComposite(
+      primary: FeedLoaderCacheDecorator(
+        decoratee: remoteFeedLoader,
+        cache: localFeedLoader),
+      fallback: localFeedLoader
+    )
+    
+    let compositeImageLoader = FeedImageDataLoaderWithFallbackComposite(
+      primary: localImageLoader,
+      fallback: FeedImageLoaderCacheDecorator(
+        decoratee: remoteImageLoader,
+        cache: localImageLoader
+      )
+    )
+    
+    window?.rootViewController = FeedUIComposer.feedComposedWith(
+      feedLoader: compositeFeedLoader,
+      imageLoader: compositeImageLoader
+    )
+  }
+  
+  func makeRemoteClient() -> HTTPClient {
+    switch UserDefaults.standard.string(forKey: "connectivity") {
+    case "offline":
+      AlwaysFailingHTTPClient()
+    default:
+      URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
+    }
   }
 }
 
+private class AlwaysFailingHTTPClient: HTTPClient {
+  private class Task: HTTPClientTask {
+    func cancel() { }
+  }
+  
+  func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) -> HTTPClientTask {
+    completion(.failure(NSError(domain: "offline", code: 0)))
+    return Task()
+  }
+}
