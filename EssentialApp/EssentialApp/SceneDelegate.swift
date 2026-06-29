bd4c0bfe8fcb4da28efe9224537e41a650c84829
Pranav Kasetti <pranavkasetti@gmail.com>
2026-06-29 17:38:32 +0100

    Configures feed navigation as window root view controller.

--- EssentialApp/EssentialApp/SceneDelegate.swift
+++ EssentialApp/EssentialApp/SceneDelegate.swift
@@ -14,6 +14,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
   func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
     guard let _ = (scene as? UIWindowScene) else { return }
     
+    configureWindow()
+  }
+  
+  func configureWindow() {
     let remoteURL = URL(string: "https://ile-api.essentialdeveloper.com/essential-feed/v1/feed")!
     let remoteClient = makeRemoteClient()
     let remoteFeedLoader = RemoteFeedLoader(
@@ -44,9 +48,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
       )
     )
     
-    window?.rootViewController = FeedUIComposer.feedComposedWith(
-      feedLoader: compositeFeedLoader,
-      imageLoader: compositeImageLoader
+    window?.rootViewController = UINavigationController(
+      rootViewController: FeedUIComposer.feedComposedWith(
+        feedLoader: compositeFeedLoader,
+        imageLoader: compositeImageLoader
+      )
     )
   }
   
